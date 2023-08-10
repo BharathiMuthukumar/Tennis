d8208285274938939fa5562499892b1cd1ed2cdd
Manibharathi-M <mani.bharathi@augustahitech.com>
2023-08-10 15:23:36 +0530

    commit

--- src/controllers/app/api/user/deleteUser/{id}/delete/index.ts
+++ src/controllers/app/api/user/deleteUser/{id}/delete/index.ts
@@ -1,42 +1,13 @@
-
 import { Request, Response, NextFunction } from "@express-types";
-import { RequestBody, PathParams, QueryParams, ResponseBody } from "./types";
-import users from '@src/models/userModel';
-import { ErrorMessageCode, logger } from "@src/utils/default";
-
+import { RequestBody, PathParams, QueryParams, ResponseBody } from "../src/types/index";
+import { logger } from "../src/utils/default";
 
 export default () =>
-	async (
-		req: Request<RequestBody, QueryParams, PathParams>,
-		res: Response<ResponseBody>,
-		next: NextFunction,
-	) => {
-  
-    
-      try {
-        const user = await users.findOneAndUpdate(
-                  {
-                    _id: req.params._id,
-                  },
-                  {
-                    $set: {
-                      isDeleted: true,
-                    },
-                  },
-                  { new: true }
-                );
-                
-        if (!user) throw Error(ErrorMessageCode.USER_NOT_FOUND);
-  
-        const resp: ResponseBody = {
-          success: true,
-          statusCode: 200,
-          successMessage: "user Account  Deleted Successfully",
-        };
-        res.status(200).json(resp);
-      } catch (err) {
-        next(err);
-        logger.error("Error while deleting user account.", err);
-      }
-  }
-  
+    async (req: Request<RequestBody, QueryParams, PathParams>, res: Response<ResponseBody>, next: NextFunction) => {
+        try {
+            return res.json({});
+        } catch (err) {
+            logger.error("error: ", err);
+            next(err);
+        }
+    };
